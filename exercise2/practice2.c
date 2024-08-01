3a85be881e56024f49463fe7b58b1bab35b466a1
miu-osako <miu_osako@saison-technology.com>
2024-08-01 17:30:25 +0900

    修正2 (#4)
    Co-authored-by: unknown <miu_oosako@S23973.hulft.saison.co.jp>

--- exercise2/practice2.c
+++ exercise2/practice2.c
@@ -2,6 +2,7 @@
 
 int main(void) {  
     printf("char=%05zu short=%05zu int=%05zu long=%05zu float=%05zu double=%05zu \n", 
-            sizeof(char), sizeof(short), sizeof(int), sizeof(long), sizeof(float), sizeof(double));
+                                            sizeof(char), sizeof(short), sizeof(int),
+                                            sizeof(long), sizeof(float), sizeof(double));
     return 0;
 }
