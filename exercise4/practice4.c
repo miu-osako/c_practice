c25b147658dcd235455849c7394455edfae6c4a1
miu_oosako <miu_osako@saison-technology.com>
2024-08-13 11:27:05 +0900

    課題４の修正１

--- exercise4/practice4.c
+++ exercise4/practice4.c
@@ -5,8 +5,8 @@ int main(void) {
     int *p;
     p = &i;
 
-    printf("i: アドレス=%p 値=%d\n", (void *)&i, i);
-    printf("p: アドレス=%p 値=%p 指す先の値=%d\n", (void *)&p, (void *)p, *p);
+    printf("i: アドレス=0x%lx 値=%d\n", (unsigned long)&i, i);
+    printf("p: アドレス=0x%lx 値=0x%lx 指す先の値=%d\n", (unsigned long)&p, (unsigned long)p, *p);
 
     return 0;
 }
