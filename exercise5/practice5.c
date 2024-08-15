1a23eb7ec318bd30efb44a38f96480b790b23018
miu_oosako <miu_osako@saison-technology.com>
2024-08-15 12:11:31 +0900

    課題5修正1

--- exercise5/practice5.c
+++ exercise5/practice5.c
@@ -2,26 +2,26 @@
 
 int main(void)
 {
-    char charArray[2] = {};
-    short shortArray[2] = {};
-    int intArray[2] = {};
-    long longArray[2] = {};
+    char char_array[2] = {};
+    short short_array[2] = {};
+    int int_array[2] = {};
+    long long_array[2] = {};
 
-    char *charPointer = charArray;
-    short *shortPointer = shortArray;
-    int *intPointer = intArray;
-    long *longPointer = longArray;
+    char *char_pointer = char_array;
+    short *short_pointer = short_array;
+    int *int_pointer = int_array;
+    long *long_pointer = long_array;
 
     printf("char配列=%lld short配列=%lld int配列=%lld long配列=%lld\n"
            "charポインタ=%lld shortポインタ=%lld intポインタ=%lld longポインタ=%lld\n",
-            (long long)(&charArray[1])-(long long)(&charArray[0]),
-            (long long)(&shortArray[1])-(long long)(&shortArray[0]),
-            (long long)(&intArray[1])-(long long)(&intArray[0]),
-            (long long)(&longArray[1])-(long long)(&longArray[0]),
-            (long long)(charArray+1)-(long long)(charArray),
-            (long long)(shortArray+1)-(long long)(shortArray),
-            (long long)(intArray+1)-(long long)(intArray),
-            (long long)(longArray+1)-(long long)(longArray));
+            (long long)(&char_array[1]) - (long long)(&char_array[0]),
+            (long long)(&short_array[1]) - (long long)(&short_array[0]),
+            (long long)(&int_array[1]) - (long long)(&int_array[0]),
+            (long long)(&long_array[1]) - (long long)(&long_array[0]),
+            (long long)(char_pointer + 1) - (long long)(char_pointer),
+            (long long)(short_pointer + 1) - (long long)(short_pointer),
+            (long long)(int_pointer + 1) - (long long)(int_pointer),
+            (long long)(long_pointer + 1) - (long long)(long_pointer));
 
     return 0;
 }
