8125e0aa46fd070f3368436d26b73956407ba25e
miu_oosako <miu_osako@saison-technology.com>
2024-08-19 16:28:42 +0900

    課題6のコードと実行結果

---
+++ exercise6/practice6.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<stddef.h>
+
+struct SAMPLE_t
+{
+    char m1;
+    short m2;
+    char m3;
+    long m4;
+    char m5;
+    void* m6;
+}a;
+
+int main(void)
+{
+    a;
+    
+    printf("構造体のサイズ=%zu メンバの合計サイズ=%zu\n"
+           "char m1オフセット=%zu サイズ=%zu\n"
+           "short m2オフセット=%zu サイズ=%zu\n"
+           "char m3オフセット=%zu サイズ=%zu\n"
+           "long m4オフセット=%zu サイズ=%zu\n"
+           "char m5オフセット=%zu サイズ=%zu\n"
+           "void* m6オフセット=%zu サイズ=%zu\n",
+           sizeof(a),
+           sizeof(a.m1)+sizeof(a.m2)+sizeof(a.m3)+sizeof(a.m4)+sizeof(a.m5)+sizeof(a.m6),
+           offsetof(struct SAMPLE_t, m1), sizeof(a.m1),
+           offsetof(struct SAMPLE_t, m2), sizeof(a.m2),
+           offsetof(struct SAMPLE_t, m3), sizeof(a.m3),
+           offsetof(struct SAMPLE_t, m4), sizeof(a.m4),
+           offsetof(struct SAMPLE_t, m5), sizeof(a.m5),
+           offsetof(struct SAMPLE_t, m6), sizeof(a.m6));
+    return 0;
+}
