3a6e82c2f947d2d0aaca1a30ce24476ea0195e72
miu_oosako <miu_osako@saison-technology.com>
2024-08-22 17:52:03 +0900

    課題7修正1

--- exercise7/practice7.c
+++ exercise7/practice7.c
@@ -4,16 +4,25 @@ static int static_counter();
 
 int main(void)
 {
-
-    for(int i = 0; i < 10; i++) {
-        printf("%d\n", static_counter());
+    for (int i = 0; i < 10; i++) {
+        int counter = static_counter();
+        printf("%d\n", counter);
     }
-    
+
     return 0; 
 }
 
-    static int static_counter()
-    {
-        static int counter = 0;
-        return counter++;
-    }
+/**
+ * @brief カウンタを返す関数
+ * 
+ * @details プログラムを起動してから呼び出すたびに1インクリメントした値を返す関数
+ * 
+ * @remark 演習で指定されているため、この関数名を使用しました。
+ * 
+ * @return 1インクリメントした値を返す
+ */
+static int static_counter()
+{
+    static int counter = 0;
+    return ++counter;
+}
