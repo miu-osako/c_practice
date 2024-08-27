20fc67f6234a431529908e5a989f29f535d080f1
miu-osako <miu_osako@saison-technology.com>
2024-08-27 11:53:56 +0900

    課題9修正1

--- exercise9/practice9.c
+++ exercise9/practice9.c
@@ -1,22 +1,32 @@
 #include<stdio.h>
 
-void swap_int(int *a, int *b)
-{
-	int tmp;
-	tmp = *a;
-	*a = *b;
-	*b = tmp;
-}
+void swap_int(int *a, int *b);
 
 int main(void)
 {
-	int u, v;
-	u = 2;
-	v = 3;
+        int u, v;
+        u = 2;
+        v = 3;
 
-	printf("u=%d v=%d\n", u, v);
-	swap_int(&u, &v);
-	printf("u=%d v=%d\n", u, v);
+        printf("u=%d v=%d\n", u, v);
+        swap_int(&u, &v);
+        printf("u=%d v=%d\n", u, v);
 
-	return 0;
+        return 0;
+}
+
+/**
+ * @brief 変数の値を入れ替える関数
+ *
+ * @details int型の2つの変数の値を入れ替える関数
+ *
+ * @return int *a と int *b の値を入れ替えた値を返す
+ */
+void swap_int(int *a, int *b)
+{
+        int tmp;
+
+	tmp = *a;
+        *a = *b;
+        *b = tmp;
 }
