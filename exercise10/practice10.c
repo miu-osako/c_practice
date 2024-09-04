836a98aadb0812bfda66276b47574b2233bc0c55
miu-osako <miu_osako@saison-technology.com>
2024-09-04 14:44:12 +0900

    課題10のコードと実行結果 (#13)
    * 課題10のコードと実行結果
    
    * 課題10修正1
    
    * 課題10修正2
    
    * 課題10修正3
    
    * 課題10修正4
    
    * 課題10修正5
    
    * 課題10修正6
    
    * 課題10修正7
    
    * 課題10修正8
    
    * 課題10修正9
    
    * 課題10修正10

---
+++ exercise10/practice10.c
@@ -0,0 +1,51 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+typedef struct DAT_t {
+	int n;
+	const char* s;
+} DAT;
+
+int compare_DAT_t(const void *a, const void *b);
+
+int main(void)
+{
+	DAT dat[] = {
+		{9,"A"},
+		{2,"A"},
+		{5,"AB"},
+		{5,"AA"},
+	};
+	size_t i;
+	
+	size_t num_dat = sizeof(dat) / sizeof(dat[0]);
+	qsort(dat, num_dat, sizeof(DAT), compare_DAT_t);
+
+	for (i = 0; i < num_dat; i++) {
+		printf("n: %d, s: %s\n", dat[i].n, dat[i].s);
+	}
+
+	return 0;
+}
+
+/**
+ * @brief struct DAT_tのint nを比較、同値の場合char* sを比較
+ *
+ * @param[in] a 大小比較の対象1
+ * @param[in] b 大小比較の対象2
+ *
+ * @return aが大きい場合正の値、bが大きい場合負の値、同じ場合0を返す
+ */
+int compare_DAT_t(const void *a, const void *b)
+{
+	const DAT *dat1 = (const DAT *)a;
+	const DAT *dat2 = (const DAT *)b;
+	
+	if (dat1->n < dat2->n) {
+		return -1;
+	} else if (dat1->n > dat2->n) {
+		return 1;
+	}
+	return strcmp(dat1->s, dat2->s);
+}
