ef2104e16428bb4449665600474b03024dce9254
miu-osako <miu_osako@saison-technology.com>
2024-09-10 14:07:54 +0900

    課題11修正4

--- exercise11/practice11.c
+++ exercise11/practice11.c
@@ -1,34 +1,40 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+#define READ_FILE_MAX 256 /*ファイル読み込み最大バイト数を定義*/
+
 int main(int argc, char *argv[])
 {
-	int max_length;
-	char str[max_length];
+	char str[READ_FILE_MAX + 1];
+	const char *infile;
 	
-	max_length= 256;
+	infile = argv[1];
 	FILE *fp = NULL;
-	
+
 	if (argc != 2) {
 		printf("usage: display_file filename\n");
 		return 1;
 	}
 
-	fp = fopen(argv[1], "r");
+	fp = fopen(infile, "r");
 	if (fp == NULL) {
-		printf("error opening file\n");
-		return -1;
+		perror("error opening file\n");
+		return 1;
 	}
 
-	while (!feof(fp)) {
-		fgets(str, max_length, fp);
+	while (fgets(str, READ_FILE_MAX + 1, fp) != NULL) {
 		printf("%s",str);
 	}
-	
-	fclose(fp);
-	if (ferror(fp)) {
-                printf("error closing file\n");
-        }
+	if (!feof(fp)) {
+		perror("error reading file\n");
+		fclose(fp);
+		return 1;
+	}
+
+	if (fclose(fp) != 0) {
+		perror("error closing file\n");
+		return 1;
+	}
 
 	return 0;
 }
