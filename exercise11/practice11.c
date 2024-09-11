570a73d36987e1392b990668278bf569f84f105a
miu-osako <miu_osako@saison-technology.com>
2024-09-11 18:00:02 +0900

    課題11修正4

--- exercise11/practice11.c
+++ exercise11/practice11.c
@@ -7,15 +7,15 @@ int main(int argc, char *argv[])
 {
 	char str[READ_FILE_MAX + 1];
 	const char *infile;
-	
-	infile = argv[1];
 	FILE *fp = NULL;
+	int rc = 0;
 
 	if (argc != 2) {
 		printf("usage: display_file filename\n");
 		return 1;
 	}
 
+	infile = argv[1];
 	fp = fopen(infile, "r");
 	if (fp == NULL) {
 		perror("error opening file\n");
@@ -23,18 +23,17 @@ int main(int argc, char *argv[])
 	}
 
 	while (fgets(str, READ_FILE_MAX + 1, fp) != NULL) {
-		printf("%s",str);
+		printf("%s", str);
 	}
 	if (!feof(fp)) {
 		perror("error reading file\n");
-		fclose(fp);
-		return 1;
+		rc = 1;
 	}
-
+	
 	if (fclose(fp) != 0) {
 		perror("error closing file\n");
 		return 1;
 	}
 
-	return 0;
+	return rc;
 }
