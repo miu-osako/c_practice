1ffa126ee1d0121620d442ccc16c7978f5351e3c
miu-osako <miu_osako@saison-technology.com>
2024-09-13 14:02:04 +0900

    課題11のコードと実行結果 (#14)
    * 課題11のコードと実行結果
    
    * 課題11修正1
    
    * 課題11修正1
    
    * 課題11修正2
    
    * 課題11修正3
    
    * 課題11修正4
    
    * 課題11修正4
    
    * 課題11修正修正5
    
    * 課題11修正6
    
    * 課題11修正7

---
+++ exercise11/practice11.c
@@ -0,0 +1,45 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<errno.h>
+
+#define READ_FILE_MAX 8192 /*ファイル読み込み最大バイト数を定義*/
+#define SUCCESS 0 /*正常終了*/
+#define FAILURE 1 /*異常終了*/
+
+int main(int argc, char *argv[])
+{
+	char str[READ_FILE_MAX];
+	const char *infile;
+	FILE *fp = NULL;
+	int file_close_status;
+	int rc = SUCCESS;
+
+	if (argc != 2) {
+		fprintf(stderr, "usage: display_file filename\n");
+		return FAILURE;
+	}
+
+	infile = argv[1];
+	fp = fopen(infile, "r");
+	if (fp == NULL) {
+		fprintf(stderr, "error opening '%s': %s\n", infile, strerror(errno));
+		return FAILURE;
+	}
+
+	while (fgets(str, sizeof(str), fp) != NULL) {
+		printf("%s", str);
+	}
+	if (ferror(fp)) {
+		perror("error reading file\n");
+		rc = FAILURE;
+	}
+	
+	file_close_status = fclose(fp);
+	if (file_close_status != 0) {
+		perror("error closing file\n");
+		return FAILURE;
+	}
+
+	return rc;
+}
